fbf6945ecf190c0971e23b7e1145ee327d2d882b
Hirooo17 <retuermahero17@gmail.com>
2025-03-31 18:25:59 +0800

    Fix import paths in App.jsx to match component file naming conventions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
-import Login from './pages/login'
-import EmailVerify from './pages/emailVerify'
-import ResetPass from './pages/resetPass'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import EmailVerify from './pages/EmailVerify'
+import ResetPass from './pages/ResetPass'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; //
 
