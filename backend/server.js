c4e482243ce703575d8709fbac2b26b643e5c65e
Hirooo17 <retuermahero17@gmail.com>
2025-03-31 22:01:58 +0800

    again and again

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import mongoose from "mongoose";
+import mongoose, { connect } from "mongoose";
 import 'dotenv/config';
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -12,44 +12,26 @@ const app = express();
 const port = process.env.PORT || 5000;
 connectDB();
 
-// Define all possible origins
+
 const allowedOrigins = [
-    'https://cleancom-k4kf.vercel.app',
-    'https://cleancom-k4kf-7ijh2m84h-heros-projects-275bb566.vercel.app',
-    // Include any other origins you might use
-];
-
-// Handle preflight requests
-app.options('*', (req, res) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-    }
-    res.status(200).end();
-});
-
-// Configure CORS for all other requests
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-    }
-    next();
-});
+    process.env.FRONT_END_URL
+]
 
 app.use(cookieParser());
+// app.use(cors({ origin: process.env.FRONT_END_URL, credentials: true }));
+app.use(cors({origin : ['https://cleancom-k4kf.vercel.app/']}));
+
+
+// api end point
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("API WORKING fine");
 });
 app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter);
+app.use('/api/user', userRouter)
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
+}); 
